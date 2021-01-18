7e73de46f12c4f13ddc71f777f4501278435f707
muhammed konyalicetin <365068@ogr.ktu.edu.tr>
2021-01-18 14:19:03 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Goster(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.Soyad + "Faiz oranınız: " + musteri.FaizOranı);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 0001;
+            musteri1.Ad = "Fatih";
+            musteri1.Soyad = "Çolak";
+            musteri1.FaizOranı = 9;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 0002;
+            musteri2.Ad = "Kerime";
+            musteri2.Soyad = "Sayar";
+            musteri2.FaizOranı = 11;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 0003;
+            musteri3.Ad = "Zafer";
+            musteri3.Soyad = "Kara";
+            musteri3.FaizOranı = 12;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.Soyad);
+                Console.WriteLine("Faiz oranınız: " + musteri.FaizOranı);
+                Console.WriteLine("-----------------");
+            }
+        }
+    }
+}
